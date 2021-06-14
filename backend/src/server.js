8a8fd51dab2f56df80d5c66e3b8b0a77e87fe2a1
ifymatics <ifymatics2004@gmail.com>
2021-06-14 12:18:47 +0100

    resolved cors at backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const api = require("./db/db");
 
 const galleryRouter = require("./routes/galleryRoutes");
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 //Api end points
